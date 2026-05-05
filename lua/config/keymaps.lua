dd6c1dbd96c885020ed99ff313512f4f0c33470c
TaisukeFujise <tafujise3502@icloud.com>
2026-05-05 14:24:44 +0900

    update key map

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,4 @@ vim.opt.timeoutlen=300
 vim.keymap.set("i", "jj", "<Esc>", {silent=true})
 vim.keymap.set("n", "ss", ":split<Return><C-w>w")
 vim.keymap.set("n", "sv", ":vsplit<Return><C-w>w")
+vim.keymap.set({ "n", "i", "v" }, "<C-s>", "<Cmd>w<CR><Esc>", { noremap = true, silent = true, desc = "Save file" })
